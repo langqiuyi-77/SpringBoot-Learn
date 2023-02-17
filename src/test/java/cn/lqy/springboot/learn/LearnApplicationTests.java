896ee3cfc3c8f360f87570026b7ef016fac3a831
40339697 <qlang01@qub.ac.uk>
2023-02-17 10:41:17 +0000

    SpringBoot基本结构

---
+++ src/test/java/cn/lqy/springboot/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lqy.springboot.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
