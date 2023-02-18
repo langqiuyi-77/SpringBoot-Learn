18c03630fa2213399f8448754a06c1808885472b
40339697 <qlang01@qub.ac.uk>
2023-02-18 07:53:42 +0000

    未完成

--- src/test/java/cn/lqy/springboot/learn/LearnApplicationTests.java
+++ src/test/java/cn/lqy/springboot/learn/LearnApplicationTests.java
@@ -6,17 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 //3.编写测试方法
 
-@SpringBootTest(classes = UserService.class)
-//这里加不加classes取决于当前测试类(UserService)所在包的位置：
-// 在引导类所在包的子包或同级则不需要加(会自动找引导类)，否则要加
+@SpringBootTest
 class LearnApplicationTests {
-
-    @Autowired
-    UserService userService;
 
     @Test
     void contextLoads() {
-        userService.add();
+        
     }
 
 }
