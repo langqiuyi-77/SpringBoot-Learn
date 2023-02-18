758b8fc5f86afa5dbea30408065071e038d803b4
40339697 <qlang01@qub.ac.uk>
2023-02-18 07:49:52 +0000

    SpringBoot整合Junit

--- src/test/java/cn/lqy/springboot/learn/LearnApplicationTests.java
+++ src/test/java/cn/lqy/springboot/learn/LearnApplicationTests.java
@@ -1,13 +1,22 @@
 package cn.lqy.springboot.learn;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//3.编写测试方法
+
+@SpringBootTest(classes = UserService.class)
+//这里加不加classes取决于当前测试类(UserService)所在包的位置：
+// 在引导类所在包的子包或同级则不需要加(会自动找引导类)，否则要加
 class LearnApplicationTests {
+
+    @Autowired
+    UserService userService;
 
     @Test
     void contextLoads() {
+        userService.add();
     }
 
 }
